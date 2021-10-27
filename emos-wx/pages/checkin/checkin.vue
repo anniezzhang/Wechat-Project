2f1b7e9199d025199febdf029f1b3cefe9e57169
anniezzhang <zhangxinyu2014@gmail.com>
2021-10-27 13:47:32 -0400

    Add files via upload

---
+++ emos-wx/pages/checkin/checkin.vue
@@ -0,0 +1,178 @@
+<template>
+	<view>
+		<camera device-position="front" flash="off" class="camera" @error="error" v-if="showCamera"></camera>
+		<image mode="widthFix" class="image" :src="photoPath" v-if="showImage"></image>
+		<view class="operate-container">
+			<button type="primary" class="btn" @tap="clickBtn":disabled="!canCheckin">{{btnText}}</button>
+			<button type="warn" class="btn" @tap="afresh":disabled="!canCheckin">Retake</button>
+		</view>
+		<view class="notice-container">
+			<text class="notice">Tips</text>
+			<text class="desc">When using face recognition attendance system, please take off any hats, sunglasses or masks.</text>
+		</view>
+	</view>
+</template>
+
+<script>
+		var QQMapWX=require('../../lib/qqmap-wx-jssdk.min.js');
+		var qqmapsdk;
+	export default {
+		data() {
+			return {
+				canCheckin:true,
+				photoPath:"",
+				btnText:"Shot",
+				showCamera:true,
+				showImage:false
+				
+			}
+		},
+		onLoad:function(){
+			qqmapsdk=new QQMapWX({
+				key:"7TWBZ-IXVWU-RYSVR-2NZKW-27IKE-GVFOB"
+			})
+		},
+		onShow:function(){
+			let that = this
+			that.ajax(that.url.validCanCheckIn,"GET",null,function(resp){
+				let msg = resp.data.msg 
+				if(msg!="Valid sign-in time"){
+					that.canCheckin = false
+					setTimeout(function(){
+						uni.showToast({
+							title:msg,
+							icon:"none"
+						},1000)
+					})
+				}
+			})
+			
+		},
+		methods: {
+			clickBtn:function(){
+				let that=this;
+				if(that.btnText=="Shot"){
+					let ctx=uni.createCameraContext();
+					ctx.takePhoto({
+						quality:"high",
+						success:function(resp){
+							console.log(resp.tempImagePath)
+							that.photoPath=resp.tempImagePath
+							that.showCamera=false
+							that.showImage=true
+							that.btnText="Sign-in"
+						}
+					})
+				}else{
+					uni.showLoading({
+						title:"Signing you in the system. Please wait a moment."
+					})
+					setTimeout(function(){
+						uni.hideLoading()
+					},5000)
+					
+					uni.getLocation({
+						type:"wgs84",
+						success: function(resp) {
+							let latitude = resp.latitude
+							let longitude = resp.longitude
+							console.log("Location is "+latitude+","+longitude)
+							qqmapsdk.reverseGeocoder({
+								location:{
+									latitude:latitude,
+									longitude:longitude
+								},
+								success:function(resp){
+									console.log(resp.result)
+									let address=resp.result.address
+									uni.uploadFile({
+										url:that.url.checkin,
+										filePath:that.photoPath,
+										name:"photo",
+										header:{
+											token:uni.getStorageSync("token")
+										},
+										formData:{
+											address:address
+										},
+										success:function(resp){
+											if(resp.statusCode == 500 && resp.data == "不存在人脸模型"){
+												uni.hideLoading()
+												uni.showModal({
+													title: "System Message",
+													content:"No face model detected in database for this user. Would you like to create one using this photo?",
+													success:function(res){
+														if(res.confirm){
+															uni.uploadFile({
+																url:that.url.createFaceModel,
+																filePath:that.photoPath,
+																name:"photo",
+																header:{
+																	token:uni.getStorageSync("token")
+																},
+																success:function(resp){
+																	if(resp.statusCode == 500){
+																		uni.showToast({
+																			title:resp.data,
+																			icon:"none"
+																		})
+																	}else if(resp.statusCode == 200){
+																		uni.showToast({
+																			title:"Face model created successfully",
+																			icon:"none"
+																		})
+																	}
+																}
+															})
+														}else if(resp.statusCode == 200){
+															let data = JSON.parse(resp.data)
+															let code = data.code
+															let msg = data.msg
+															if(code == 200){
+																uni.hideLoading()
+																uni.showToast({
+																	title:"Signed in successfully",
+																	complete:function(){
+																		uni.navigateTo({
+																			url:"../checkin_result/checkin_result"
+																		})
+																	}
+																})
+															}
+														}else if(resp.statusCode == 500){
+															uni.showToast({
+																title:resp.data,
+																icon:"none"
+															})
+														}
+														
+													}
+												})
+											}
+										}
+										
+									})
+																	
+									
+									
+									
+								}
+							})							
+						}
+					})
+				}
+			},
+			afresh:function(){
+				let that=this;
+				that.showCamera=true;
+				that.showImage=false;
+				that.btnText="Shot"
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("checkin.less");
+
+</style>
---
+++ emos-wx/pages/checkin_result/checkin_result.vue
@@ -0,0 +1,140 @@
+<template>
+	<view class = "page">
+		<view class = "summary-container">
+			<view class = "user-info">
+				<image :src = "photo" mode = "widthFix" class="photo"></image>
+				<view class = "info">
+					<text class = "username">{{name}}</text>
+					<text class = "dept">{{deptName == null?"":deptName}}</text>
+				</view>
+			</view>
+			<view class = "date">{{date}}</view>
+		</view>
+		<view class="result-container">
+			<view class="left">
+				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+				<view class="line"></view>
+				<image src="../../static/icon-6.png" mode="widthFix" class="icon-timer"></image>
+				
+			</view>
+			<view class="right">
+				<view class="row">
+					<text class="start">Work Start（{{ attendanceTime }}）</text>
+				</view>
+				<view class="row">
+					<text class="checkin-time">Sign-in time（{{ checkinTime }}）</text>
+					<text class="checkin-result green" v-if="status=='Attended'">{{status}}</text>
+					<text class="checkin-result yellow" v-if="status=='Late'">{{status}}</text>
+				</view>
+				<view class="row">
+					<image src="../../static/icon-7.png" mode="widthFix" class="icon-small"></image>
+					<text class="desc">{{address}}</text>
+				</view>
+				<view class="row">			
+					<text class="desc">Your state new COVID cases today:</text>
+					<text class="checkin-result green" v-if="cases<=2000">{{cases}}</text>
+					<text class="checkin-result yellow" v-if="cases<=5000 && cases>2000">{{cases}}</text>
+					<text class="checkin-result red" v-if="cases>5000">{{cases}}</text>
+				</view>
+				<view class="row">
+					<image src="../../static/icon-8.png" mode="widthFix" class="icon-small"></image>
+					<text class="desc">Indentification</text>
+					<text class="checkin-result green">Passed</text>
+				</view>
+				<view class="row">
+					<text class="end">Finish（{{ closingTime }}）</text>
+				</view>
+			</view>
+		</view>
+		<view class="checkin-report">
+			<image src="../../static/big-icon-1.png" mode="widthFix" class="big-icon"></image>
+			<view class="report-title">
+				<text class="days">{{checkinDays}}</text>
+				<text class="unit">days</text>
+			</view>
+			<view class="sub-title">
+				<text>Total Sign-in</text>
+				<view class="line"></view>
+			</view>
+			<view class="calendar-container">
+				<view class="calendar" v-for="one in weekCheckin" :key="one">
+					<image src="../../static/icon-9.png" mode="widthFix" class="calendar-icon" v-if="one.type=='workday'"></image>
+					<image src="../../static/icon-10.png" mode="widthFix" class="calendar-icon" v-if="one.type=='holiday'"></image>
+					<text class="day">{{one.day}}</text>
+					<text class="result green" v-if="one.status=='Attended'">{{one.status}}</text>
+					<text class="result yellow" v-if="one.status=='Late'">{{one.status}}</text>
+					<text class="result red" v-if="one.status=='Absent'">{{one.status}}</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				name:"",
+				photo:"",
+				deptName:"",
+				address:"",
+				cases:0,
+				checkinTime:"",
+				status:"",
+				date:"",
+				attendanceTime:"",
+				closingTime:"",
+				checkinDays:0,
+				weekCheckin:[]
+				
+				/*name:"SleepyHead",
+				photo:"https://thirdwx.qlogo.cn/mmopen/vi_32/ajNVdqHZLLBFckht4kIvJyictpejiaqBLiaCwQ1cqribYibPCB0vh6uxOH9phupCVKyQwWgiaHOlAzKuKn1eC3mLWIUw/132",
+				deptName:"HR",
+				address:"Jersey City New Jersey",
+				cases:2061,
+				checkinTime:"08:45",
+				status:"Attended",
+				date:"10/12/2021",
+				attendanceTime:"9:00",
+				closingTime:"17:00",
+				checkinDays:201,
+				weekCheckin:[
+					{type:"workday",day:"Mon",status:"Absent"},
+					{type:"workday",day:"Tue",status:"Late"},
+					{type:"workday",day:"Wed",status:"Attended"},
+					{type:"workday",day:"Thur",status:"Attended"},
+					{type:"workday",day:"Fri",status:"Attended"},
+					{type:"holiday",day:"Sat",status:""},
+					{type:"holiday",day:"Sun",status:""},
+				]*/
+				
+			}
+		},
+		onShow:function(){
+			let that=this
+			that.ajax(that.url.searchTodayCheckin,"GET",null,function(resp){
+				let result=resp.data.result
+				that.name=result.name
+				that.photo=result.photo
+				that.deptName=result.deptName
+				that.address=result.address
+				that.cases=result.risk
+				that.checkinTime=result.checkinTime
+				that.status=result.status
+				that.date=result.date
+				that.attendanceTime=result.attendanceTime
+				that.closingTime=result.closingTime
+				that.checkinDays=result.checkinDays
+				that.weekCheckin=result.weekCheckin
+			})
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("checkin_result.less");
+
+</style>
